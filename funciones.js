8e8c33dcfeb4c2dc3e5138331137d9ef0591983c
githubfacu <elorzfacundo@gmail.com>
2024-04-13 20:11:36 -0300

    marca de practicas en curso

--- funciones.js
+++ funciones.js
@@ -1,15 +1,24 @@
 const material = [
     {
+        id: 7,
+        titulo: 'Next.js | Tailwind CSS',
+        fecha: 'Abril 2024',
+        enCurso: true,
+        proyectos: [
+        ]
+    },
+    {
         id: 6,
         titulo: 'Simulación Laboral | No Country',
         fecha: 'Abril 2024',
+        enCurso: true,
         proyectos: [
             {
                 proyectoId: 1,
                 ruta: '',
                 anclaTexto: 'RecursosHumanosWebApp',
                 img: '',
-                descripcion: '4 sprints, 4 semanas, 10 integrantes. Rol Frontend QA',
+                descripcion: '4 sprints, 4 semanas, 10 integrantes. Rol Frontend QA React + Tailwind',
                 personal: false
             }
         ]
@@ -31,7 +40,7 @@ const material = [
     },
     {
         id: 4,
-        titulo: '&#60; / &#62;',
+        titulo: '< / >',
         fecha: 'Enero 2024',
         proyectos: [
             {
@@ -39,7 +48,7 @@ const material = [
                 ruta: 'https://arcade-game-vercel.vercel.app',
                 anclaTexto: 'ArcadeGame',
                 img: './Imagenes/capturas/Captura_arcade-game-vercel.vercel.app.jpeg',
-                descripcion: 'Primera práctica con chatGPT, React.',
+                descripcion: 'Primera práctica con chatGPT. React.',
                 personal: true
             },
             {
@@ -70,7 +79,7 @@ const material = [
                 ruta: 'https://psycho-pass.vercel.app',
                 anclaTexto: 'TrabajoPráctico',
                 img: './Imagenes/capturas/Captura_psycho-pass.vercel.app.jpeg',
-                descripcion: 'Trabajo práctico en dupla, React.',
+                descripcion: 'Trabajo práctico en dupla. React.',
                 personal: false
             }            
         ]
@@ -118,29 +127,86 @@ const material = [
 
 const $lineadetiempo = document.getElementById('lineaDeTiempo')
 
+agregarSeccion()
 agregarMaterial()
 agregarProyectos()
+hoverMsj()
 
-function agregarMaterial(){
+
+function agregarSeccion(){
 
     material.forEach(materia => {
         return $lineadetiempo.innerHTML+=`
-        <section>
-            <h3>${materia.titulo}</h3>
-            <h4>${materia.fecha}</h4>
-            <div class='linksDiv' id='${materia.id}'></div>
-        </section>`
-    })    
+        <section id='${materia.id}'></section>`
+    })
+}
+
+
+function agregarMaterial(){
+
+    const $sectionDiv = document.querySelectorAll('#lineaDeTiempo section')
+
+    $sectionDiv.forEach(sectionHTML=>{
+
+        material.forEach(materia => {
+            if (parseInt(sectionHTML.id) === materia.id) {
+
+                const tituloCard = document.createElement('div')
+                tituloCard.classList.add('tituloCard')
+
+                const enCursoIndice = document.createElement('div')
+                enCursoIndice.setAttribute('id', materia.id)
+                enCursoIndice.classList.add('enCurso')
+
+                const enCursoMsj = document.createElement('h5')
+                enCursoMsj.setAttribute('id', materia.id)
+                enCursoMsj.innerText='En curso'
+
+                const titulo = document.createElement('h3')
+                titulo.innerText=`${materia.titulo}`
+
+                if(materia.enCurso){
+                    tituloCard.appendChild(enCursoIndice)
+                    tituloCard.appendChild(enCursoMsj)
+                    titulo.classList.add('tituloMargin')
+                }
+
+                tituloCard.appendChild(titulo)
+
+                sectionHTML.appendChild(tituloCard)
+
+                return sectionHTML.innerHTML+=`
+                <h4>${materia.fecha}</h4>
+                <div class='linksDiv' id='${materia.id}'></div>`
+            }
+        })
+    })
+}
+
+function hoverMsj(){
+    const $msjDiv = document.querySelectorAll('.enCurso')
+    const enCursoMsj = document.querySelectorAll('.tituloCard h5')
+
+    $msjDiv.forEach(elemento=>{
+        enCursoMsj.forEach(msj=>{
+            if(msj.id === elemento.id){
+                elemento.addEventListener('mouseover',function(){
+                msj.style.opacity='1'
+                })
+                elemento.addEventListener('mouseout',function(){
+                    msj.style.opacity='0'
+                })                
+            }
+        })
+    })
 }
 
 
 function agregarProyectos(){
 
-    const $linksDiv = document.querySelectorAll('.linksDiv')
-
+    const $linksDiv = document.querySelectorAll('.linksDiv') 
 
     $linksDiv.forEach(linksDivHTML=>{
-        console.log(linksDivHTML.id)
 
         material.forEach(materia => {
 
@@ -181,8 +247,7 @@ function agregarProyectos(){
                             practicaDiv.style.display='none'
                         })
 
-                        linksDivHTML.appendChild(practicaDiv)
-
+                        return linksDivHTML.appendChild(practicaDiv)
                     })
                 }
             }
