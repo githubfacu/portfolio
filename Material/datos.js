ca8499b6d9e7f75bd7b33dfff40c3025582d8485
githubfacu <elorzfacundo@gmail.com>
2024-06-06 15:45:37 -0300

    material folder

---
+++ Material/datos.js
@@ -0,0 +1,203 @@
+export const material = [
+    // {
+    //     id: 10,
+    //     titulo: '<span>Next.js | <h4>Udemy</h4></span>',
+    //     fecha: 'Junio 2024',
+    //     enCurso: true,
+    //     proyectos: [
+    //         {
+    //             proyectoId: 1,
+    //             ruta: '',
+    //             anclaTexto: 'E-commerce',
+    //             img: './Imagenes/capturas/fondo_negro.png',
+    //             descripcion: 'NextJS 14. Guía completa con E-commerce',
+    //             personal: false,
+    //             typescript: true
+    //         }
+    //     ]
+    // },
+    {
+        id: 9,
+        titulo: '<span>Next.js App Router course | <h4>NEXT.js (Vercel)</h4></span>',
+        fecha: 'Mayo 2024',
+        enCurso: false,
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://github.com/githubfacu/Learn_Next.js',
+                anclaTexto: 'Learn_Next.js',
+                img: './Imagenes/capturas/fondo_negro.png',
+                descripcion: 'React Foundations Course (+vitest) & Financial Dashboard',
+                personal: false,
+                typescript: true
+            }
+        ]
+    },
+    {
+        id: 8,
+        titulo: '<span>React Advanced | <h4>Rolling Code</h4></span>',
+        fecha: 'Mayo 2024',
+        enCurso: true,
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://rolling-code-nav-cards.vercel.app/',
+                anclaTexto: 'HeroCardsNav',
+                img: './Imagenes/capturas/Captura_rolling-code-nav-cards.jpeg',
+                descripcion: 'Práctica en grupo. React + Ant Design UI components',
+                personal: false,
+                typescript: false
+            },
+            {
+                proyectoId: 2,
+                ruta: 'https://reducer-nine.vercel.app/',
+                anclaTexto: 'ReducerPalet',
+                img: './Imagenes/capturas/Captura_Reducer.jpeg',
+                descripcion: 'useReducer, Ant design +Tailwind +TypeScript',
+                personal: true,
+                typescript: true
+            },
+        ]
+    },
+    {
+        id: 7,
+        titulo: '<span>Next.js <h4>+TypeScript +Tailwind</h4> | <h4>Digital House</h4></span>',
+        fecha: 'Abril 2024',
+        enCurso: false,
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://github.com/githubfacu/Red_Social_Next.js',
+                anclaTexto: 'RedSocial',
+                img: './Imagenes/capturas/fondo_negro.png',
+                descripcion: 'WebApp dictada. Next.js +Tailwind +TypeScript',
+                personal: false,
+                typescript: true
+            },
+            {
+                proyectoId: 2,
+                ruta: 'https://lector-transcripcion-de-llamadas.vercel.app/',
+                anclaTexto: 'LectorTrancripcion',
+                img: './Imagenes/capturas/Captura_lector-transcripcion-de-llamadas.vercel.app.jpeg',
+                descripcion: 'Prueba técnica. Next.js +Tailwind +TypeScript',
+                personal: false,
+                typescript: true
+            }
+        ]
+    },
+    {
+        id: 6,
+        titulo: '<span>Simulación Laboral | <h4>No Country</h4></span>',
+        fecha: 'Abril 2024',
+        enCurso: false,
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://gestion-de-recursos-humanos-en-la-nube-facundodevs-projects.vercel.app',
+                anclaTexto: 'RRHH-WebAppMobile',
+                img: './Imagenes/capturas/Captura_rrhh.jpeg',
+                descripcion: '4 sprints, 4 semanas, 10 integrantes. Frontend QA. React + Tailwind.',
+                personal: false
+            }
+        ]
+    },
+    {
+        id: 5,
+        titulo: '<span>Proyecto Integrador | <h4>Digital House</h4></span>',
+        fecha: 'Febrero 2024',
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://gitlab.com/mtimoxenko/integrador',
+                anclaTexto: 'BookingApp',
+                img: './Imagenes/capturas/Captura-CarRental2.jpeg',
+                descripcion: 'Proyecto fin de curso, 8 integrantes. Frontend junto a Amy y Mateo.',
+                personal: false
+            }
+        ]
+    },
+    {
+        id: 4,
+        titulo: '< / >',
+        fecha: 'Enero 2024',
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://arcade-game-vercel.vercel.app',
+                anclaTexto: 'ArcadeGame',
+                img: './Imagenes/capturas/Captura_arcade-game-vercel.vercel.app.jpeg',
+                descripcion: 'Práctica de React. Responsive.',
+                personal: true
+            },
+            {
+                proyectoId: 2,
+                ruta: 'https://api-football-omega.vercel.app',
+                anclaTexto: 'API-FOOTBALL',
+                img: './Imagenes/capturas/Captura_api-football-omega.vercel.app.jpeg',
+                descripcion: 'Peticiones... React.',
+                personal: true
+            }            
+        ]
+    },
+    {
+        id: 3,
+        titulo: '<span>React | <h4>Digital House</h4></span>',
+        fecha: 'Octubre 2023',
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://githubfacu.github.io/f1_temporada_23/',
+                anclaTexto: 'F1-2023',
+                img: './Imagenes/capturas/Captura_f1.jpeg',
+                descripcion: 'Probando ECMAScript6. API-Fetch 1.',
+                personal: true
+            },
+            {
+                proyectoId: 2,
+                ruta: 'https://psycho-pass.vercel.app',
+                anclaTexto: 'TrabajoPráctico',
+                img: './Imagenes/capturas/Captura_psycho-pass.vercel.app.jpeg',
+                descripcion: 'Trabajo práctico en dupla. React.',
+                personal: false
+            }            
+        ]
+    },
+    {
+        id: 2,
+        titulo: '<span>JavaScript | <h4>Digital House</h4></span>',
+        fecha: 'Abril 2023',
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://githubfacu.github.io/reproductor_de_musica_js/',
+                anclaTexto: 'ReproductorDeMúsica',
+                img: './Imagenes/capturas/Captura_reproductor_musica.jpeg',
+                descripcion: 'Primer práctica personal, JavaScript vanilla.',
+                personal: true
+            },
+            {
+                proyectoId: 2,
+                ruta: 'https://githubfacu.github.io/interactive_magazine/',
+                anclaTexto: 'InteractiveMagazine',
+                img: './Imagenes/capturas/Captura_magazine.jpeg',
+                descripcion: 'Interactive Magazine, JavaScript vanilla.',
+                personal: true
+            }
+        ]
+    },
+    {
+        id: 1,
+        titulo: '<span>HTML CSS | <h4>Digital House</h4></span>',
+        fecha: 'Agosto 2022',
+        proyectos: [
+            {
+                proyectoId: 1,
+                ruta: 'https://githubfacu.github.io/frontend_uno_page/',
+                anclaTexto: 'TrabajoPráctico',
+                img: './Imagenes/capturas/Captura_maquetacion_front1.jpeg',
+                descripcion: 'Maquetación básica sin funciones.',
+                personal: false
+            }
+        ]
+    }
+]
