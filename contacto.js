7dcfa1d2d8431938c02ffdf443b6a5acb14e9653
githubfacu <elorzfacundo@gmail.com>
2025-08-11 21:28:08 -0300

    feat: social-iconos style classes

--- contacto.js
+++ contacto.js
@@ -1,5 +1,8 @@
-const $divIcons = document.querySelector('.social-iconos')
-const $mailIcon = document.querySelector('.social-iconos span')
+const $divIcons = document.querySelector('.social-iconos-on-desktop')
+const $divIconsMobile = document.querySelector('.social-iconos')
+
+const $mailIcon = document.querySelector('.social-iconos-on-desktop span')
+const $mailIconMobile = document.querySelector('.social-iconos span')
 
 const miMail = document.createElement('h5')
 const copiedMessage = document.createElement('h4')
@@ -11,8 +14,21 @@ copiedMessage.innerText='Copiado en portapapeles'
 copiedMessage.style.display='none'
 
 $divIcons.appendChild(copiedMessage)
+$divIconsMobile.appendChild(copiedMessage)
 
 $mailIcon.addEventListener('click', function() {
+    navigator.clipboard.writeText(miMail.innerText)
+
+    copiedMessage.style.display='block'
+
+    const timer = setTimeout(() => {
+        copiedMessage.style.display='none'
+    }, 2000);
+
+    return () => clearTimeout(timer)
+})
+
+$mailIconMobile.addEventListener('click', function() {
     navigator.clipboard.writeText(miMail.innerText)
 
     copiedMessage.style.display='block'
