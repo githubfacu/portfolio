768b109d7a2d32e858a951d8ca0597b12266f91a
githubfacu <elorzfacundo@gmail.com>
2025-12-07 00:11:08 -0300

    feat: basePath url

--- Paginas/scripts/estudios.js
+++ Paginas/scripts/estudios.js
@@ -4,27 +4,21 @@ import "../../subMenu.js";
 
 import { renderCourses } from "../../courses.js";
 
-const accesibilidadFile = '../../Material/courses/accesibilidad-courses.json';
-const anexoFile = '../../Material/courses/anexo-courses-data.json';
+const isLocal = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
 
-// --- Request Accesibilidad ---
-const requestAcc = new XMLHttpRequest();
-requestAcc.overrideMimeType('application/json');
-requestAcc.open('GET', accesibilidadFile);
-requestAcc.onload = function () {
-    const data = JSON.parse(this.responseText);
-    renderCourses(data);
-};
-requestAcc.onerror = (err) => console.error(err);
-requestAcc.send();
+const BASE_PATH = isLocal
+    ? ''
+    : '/portfolio';
 
-// --- Request Anexo ---
-const requestAnexo = new XMLHttpRequest();
-requestAnexo.overrideMimeType('application/json');
-requestAnexo.open('GET', anexoFile);
-requestAnexo.onload = function () {
-    const data = JSON.parse(this.responseText);
-    renderCourses(data);
-};
-requestAnexo.onerror = (err) => console.error(err);
-requestAnexo.send();
+const accesibilidadFile = `${BASE_PATH}/Material/courses/accesibilidad-courses.json`;
+const anexoFile = `${BASE_PATH}/Material/courses/anexo-courses-data.json`;
+
+fetch(accesibilidadFile)
+    .then(res => res.json())
+    .then(renderCourses)
+    .catch(console.error);
+
+fetch(anexoFile)
+    .then(res => res.json())
+    .then(renderCourses)
+    .catch(console.error);
