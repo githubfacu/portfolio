7b1e49d88264ee6307d605a7463df598c45ce81e
githubfacu <elorzfacundo@gmail.com>
2025-12-07 00:03:18 -0300

    feat: fetching data ruta

--- Paginas/scripts/estudios.js
+++ Paginas/scripts/estudios.js
@@ -4,15 +4,27 @@ import "../../subMenu.js";
 
 import { renderCourses } from "../../courses.js";
 
-const accesibilidadFile = '../../Material/courses/accesibilidad-courses.json'
-const anexoFile = '../../Material/courses/anexo-courses-data.json'
+const accesibilidadFile = '../../Material/courses/accesibilidad-courses.json';
+const anexoFile = '../../Material/courses/anexo-courses-data.json';
 
-fetch(anexoFile)
-    .then(res => res.json())
-    .then(renderCourses)
-    .catch(console.error);
+// --- Request Accesibilidad ---
+const requestAcc = new XMLHttpRequest();
+requestAcc.overrideMimeType('application/json');
+requestAcc.open('GET', accesibilidadFile);
+requestAcc.onload = function () {
+    const data = JSON.parse(this.responseText);
+    renderCourses(data);
+};
+requestAcc.onerror = (err) => console.error(err);
+requestAcc.send();
 
-fetch(accesibilidadFile)
-    .then(res => res.json())
-    .then(renderCourses)
-    .catch(console.error);
+// --- Request Anexo ---
+const requestAnexo = new XMLHttpRequest();
+requestAnexo.overrideMimeType('application/json');
+requestAnexo.open('GET', anexoFile);
+requestAnexo.onload = function () {
+    const data = JSON.parse(this.responseText);
+    renderCourses(data);
+};
+requestAnexo.onerror = (err) => console.error(err);
+requestAnexo.send();
