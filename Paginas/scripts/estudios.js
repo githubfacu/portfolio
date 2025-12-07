d6e2492ee7636bc63b8bbfa7096c0e4137048fcf
githubfacu <elorzfacundo@gmail.com>
2025-12-06 23:53:33 -0300

    feat: fetching, order data

--- Paginas/scripts/estudios.js
+++ Paginas/scripts/estudios.js
@@ -4,8 +4,8 @@ import "../../subMenu.js";
 
 import { renderCourses } from "../../courses.js";
 
-const accesibilidadFile = '../Material/courses/accesibilidad-courses.json'
-const anexoFile = '../Material/courses/anexo-courses-data.json'
+const accesibilidadFile = '../../Material/courses/accesibilidad-courses.json'
+const anexoFile = '../../Material/courses/anexo-courses-data.json'
 
 fetch(anexoFile)
     .then(res => res.json())
